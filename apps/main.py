cbfa2978da1e5bd53b046dac54c27bbbc6d1d4bd
mago960806 <mago960806@hotmail.com>
2018-06-14 09:31:50 +0800

    修改了部分功能

--- apps/main.py
+++ apps/main.py
@@ -9,6 +9,12 @@ app.config.from_object(DevelopmentConfig)
 #把app对象传给SQLAlchemy来初始化
 db = SQLAlchemy(app)
 
+
+tag = db.Table('post_tags',
+    db.Column('post_id', db.Integer, db.ForeignKey('PostTable.id'))
+    db.Column('tag_id', db.Integer, db.ForeignKey('TagTable.id'))
+)
+
 class User(db.Model):
     __tablename__ = 'UserTable'
     id = db.Column(db.Integer(), primary_key=True)
@@ -34,13 +40,34 @@ class Post(db.Model):
     text = db.Column(db.Text())
     create_date = db.Column(db.DateTime())
     update_date = db.Column(db.DateTime())
+    comments = db.relationship(
+        'Comment',
+        backref='post',
+        lazy='dynamic'
+    )
     user_id = db.Column(db.Integer(), db.ForeignKey('UserTable.id'))
-    def __init__(self, username):
-        self.username = username
+    tags = db.relationship(
+        'Tag',
+        backref='post',
+        lazy='dynamic'
+    )
+
+    def __init__(self, title):
+        self.title = title
     
     def __repr__(self):
-        return "<User '{}'>".format(self.username)
+        return "<Post '{}'>".format(self.title)
 
+class Comment(db.Model):
+    __tablename__ = 'CommentTable'
+    id = db.Column(db.Integer(), primary_key=True)
+    name = db.Column(db.String(256))
+    text = db.Column(db.Text())
+    date = db.Column(db.DateTime())
+    post_id = db.Column(db.Integer(), db.ForeignKey('PostTable.id'))
+
+    def __repr__(self):
+        return "<Comment '{}'>".format(self.text[:15])
 
 @app.route('/')
 def index():
